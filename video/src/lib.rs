b90dbed943f71a49dc4a530c2e095b331b8688b3
snylonue <snylonue@gmail.com>
2019-11-29 21:33:07 +0800

    use u64, String instead of Epinfo

--- video/src/lib.rs
+++ video/src/lib.rs
@@ -7,6 +7,7 @@ use std::collections::hash_map;
 
 use ngtools;
 use basetask;
+use basetask::Tid;
 use basetask::Modify;
 use basetask::Read;
 
@@ -18,20 +19,17 @@ pub enum Status {
     Hold,
     Dropped,
 }
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Hash, Clone)]
-pub struct EpInfo {
-    pub number: String,
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+pub struct Episode {
+    number: u64,
+    pub chap: String,
+    pub name: String,
+    pub status: Status,
     pub ep_type: String,
 }
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
-pub struct Episode {
-    chap: EpInfo,
-    pub name: String,
-    pub status: Status,
-}
-#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub struct Episodes {
-    eps: HashMap<EpInfo, Episode>,
+    eps: HashMap<u64, Episode>,
 }
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub struct Video {
@@ -43,48 +41,34 @@ pub struct Video {
     tid: u64,
 }
 
-impl EpInfo {
-    pub fn new<S>(number:S, ep_type:S) -> EpInfo
-        where S: Into<String>
-    {
-        EpInfo { number: number.into(), ep_type: ep_type.into() }
-    }
-}
 impl Episode {
-    pub fn new<S>(chap: EpInfo, name: S, status: Status) -> Episode
+    pub fn new<S>(chap:S, name: S, status: Status, ep_type: S) -> Episode
         where S: Into<String>
     {
-        Episode { chap, name:name.into(), status }
+        Episode { number: ngtools::random_hash(), chap: chap.into(), name: name.into(), status, ep_type: ep_type.into() }
     }
-    pub fn number(&self) -> &String {
-        &self.chap.number
+    pub fn number(&self) -> u64 {
+        self.number
     }
-    pub fn set_number<S>(&mut self, new_num: S) 
-        where S: Into<String>
+    pub fn set_number(&mut self, new_num: u64) 
     {
-        self.chap.number = new_num.into();
-    }
-    pub fn ep_type(&self) -> &String {
-        &self.chap.ep_type
-    }
-    pub fn set_ep_type<S>(&mut self, new_ep_type: S) 
-        where S: Into<String>
-    {
-        self.chap.ep_type = new_ep_type.into();
-    }
-    pub fn chap(&self) -> &EpInfo {
-        &self.chap
-    }
-
-}
-impl Default for EpInfo {
-    fn default() -> EpInfo {
-        EpInfo::new("1", "ep")
+        self.change_tid_v(new_num);
     }
 }
 impl Default for Episode {
     fn default() -> Episode {
-        Episode::new(EpInfo::default(), "", Status::Watching)
+        Episode::new("1", "", Status::Watching, "ep")
+    }
+}
+impl Tid for Episode {
+    fn tid(&self) -> u64 {
+        self.number
+    }
+    fn change_tid(&mut self) {
+        self.number = ngtools::random_hash();
+    }
+    fn change_tid_v(&mut self, tid: u64) {
+        self.number = tid;
     }
 }
 impl Episodes {
@@ -98,7 +82,7 @@ impl Episodes {
         }
         eps
     }
-    pub fn types(&self) -> hash_map::Keys<EpInfo, Episode> {
+    pub fn types(&self) -> hash_map::Keys<u64, Episode> {
         self.eps.keys()
     }
     pub fn len(&self) -> usize {
@@ -107,10 +91,10 @@ impl Episodes {
 }
 impl Modify for Episodes {
     type Task = Episode;
-    type Key = EpInfo;
+    type Key = u64;
 
     fn insert(&mut self, new_task: Self::Task) -> Option<Self::Task> {
-        self.eps.insert(new_task.chap().clone(), new_task)
+        self.eps.insert(new_task.number, new_task)
     }
     fn pop(&mut self, key: &Self::Key) -> Option<Self::Task> {
         self.eps.remove(key)
@@ -118,7 +102,7 @@ impl Modify for Episodes {
 }
 impl Read for Episodes {
     type Task = Episode;
-    type Key = EpInfo;
+    type Key = u64;
 
     fn get(&self, key: &Self::Key) -> Option<&Self::Task> {
         self.eps.get(key)
@@ -139,7 +123,7 @@ impl Video {
         Video { name: name.into(), eps, status, progress, create_time, tid }
     }
 }
-impl basetask::Tid for Video {
+impl Tid for Video {
     fn tid(&self) -> u64 {
         self.tid
     }
