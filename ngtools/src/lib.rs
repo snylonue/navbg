0c8f6b0866927c2a29b575cedd80349955dadf12
snylonue <snylonue@gmail.com>
2019-12-04 21:12:14 +0800

    add const for some methods

--- ngtools/src/lib.rs
+++ ngtools/src/lib.rs
@@ -5,7 +5,6 @@ use rand::Rng;
 use serde_json;
 use serde::Serialize;
 use serde::Deserialize;
-use serde::Deserializer;
 
 const LETTERS: [char; 62] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
@@ -52,13 +51,13 @@ impl Progress {
             Ok(())
         }
     }
-    pub fn finished(&self) -> u32 {
+    pub const fn finished(&self) -> u32 {
         self.finished
     }
-    pub fn total(&self) -> u32 {
+    pub const fn total(&self) -> u32 {
         self.total
     }
-    pub fn status(&self) -> (u32, u32) {
+    pub const fn status(&self) -> (u32, u32) {
         (self.finished, self.total)
     }
     pub const fn is_finished(&self) -> bool {
